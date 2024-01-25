649ec88f291c7a542ca59209c3f8e7416724128a
logicguy <logicguy.mailandcontact@gmail.com>
2024-01-25 13:53:19 +0100

    Moreeeeeeee

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
 #include <gtk/gtk.h>
 #include <sqlite3.h>
 
@@ -42,33 +44,75 @@ const char* data = "Callback function called";
 static void closeDB()
 {
   sqlite3_close(db);
-  gtk_label_set_text((GtkLabel *) label, "Database closed succesfully!");
+  printf("Database closed succesfully!");
+}
+
+static int getRecipesCallback(void *data, int argc, char **argv, char **azColName){
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]: %s\n", i, argv[i]);
+  }
+
+  gtk_tree_store_append (store, &iter2, &iter1);  /* Acquire a child iterator */
+  gtk_tree_store_set (store, &iter2,
+                      TITLE_COLUMN, argv[1],
+                      DIETARRY_COLUMN, argv[2],
+                      -1);
 }
 
 static int getCatsCallback(void *data, int argc, char **argv, char **azColName){
-  int i;
-  for(i = 0; i<argc; i++){
-    gtk_tree_store_append (store, &iter1, NULL);  /* Acquire a top-level iterator */
-    gtk_tree_store_set (store, &iter1,
-                        TITLE_COLUMN, "",
-                        CATEGORY_COLUMN, argv[i],
-                        DIETARRY_COLUMN, "",
-                        -1);
+
+  gtk_tree_store_append (store, &iter1, NULL);  /* Acquire a top-level iterator */
+  gtk_tree_store_set (store, &iter1,
+                      TITLE_COLUMN, "",
+                      CATEGORY_COLUMN, argv[1],
+                      DIETARRY_COLUMN, "",
+                      -1);
+
+  const char *staticSQL = "SELECT \
+  recipe.id, \
+  recipe.name, \
+  IFNULL( \
+    group_concat(dietarry_requirements.name, ', '), '' \
+  ) AS req_names \
+FROM \
+  recipe \
+  LEFT JOIN recipe_requirements_association ON recipe.id = recipe_requirements_association.recipe_id \
+  LEFT JOIN dietarry_requirements ON recipe_requirements_association.requirement_id = dietarry_requirements.id \
+WHERE \
+	recipe.category = %s \
+GROUP BY \
+  recipe.id, recipe.name;";
+
+  size_t resultLength = strlen(staticSQL) + strlen(argv[0]);
+  char *sql = malloc(resultLength + 1); // +1 for the null terminator
+  if (sql != NULL) {
+    sprintf(sql, staticSQL, argv[0]);
+  } else {
+    printf("Memory allocation failed\n");
   }
+  rc = sqlite3_exec(db, sql, getRecipesCallback, (void*)data, &zErrMsg);
+
+  if( rc != SQLITE_OK ) {
+    sqlite3_free(zErrMsg);
+  } else {
+    printf("Data retrived succesfully!\n");
+  }
+  free(sql);
+
   return 0;
 }
 
 static void getCats()
 {
-    sql = "SELECT name FROM categories;";
-    rc = sqlite3_exec(db, sql, getCatsCallback, (void*)data, &zErrMsg);
+  sql = "SELECT id, name FROM categories;";
+  rc = sqlite3_exec(db, sql, getCatsCallback, (void*)data, &zErrMsg);
 
-    if( rc != SQLITE_OK ) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    } else {
-        gtk_label_set_text((GtkLabel *) label, "Data retrived succesfully!");
-    }
+  if( rc != SQLITE_OK ) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  } else {
+    printf("Data retrived succesfully!\n");
+  }
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -142,6 +186,7 @@ static void populate_tree_model (GtkTreeStore *store)
 static void activate (GtkApplication *app, gpointer user_data)
 {
 
+  printf("Getting DB");
   rc = sqlite3_open("src/database.db", &db);
 
   /* create a new window, and set its title */
@@ -184,30 +229,30 @@ static void activate (GtkApplication *app, gpointer user_data)
                 "foreground", "red",
                 NULL);
 
-   /* Create a column, associating the "text" attribute of the
-   * cell_renderer to the first column of the model */
-   column = gtk_tree_view_column_new_with_attributes ("Category", renderer,
-                                                      "text", CATEGORY_COLUMN,
-                                                      NULL);
+  /* Create a column, associating the "text" attribute of the
+  * cell_renderer to the first column of the model */
+  column = gtk_tree_view_column_new_with_attributes ("Category", renderer,
+                                                     "text", CATEGORY_COLUMN,
+                                                     NULL);
 
-   /* Add the column to the view. */
-   gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  /* Add the column to the view. */
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
 
-   /* Second column.. title of the book. */
-   renderer = gtk_cell_renderer_text_new ();
-   column = gtk_tree_view_column_new_with_attributes ("Recipe",
-                                                      renderer,
-                                                      "text", TITLE_COLUMN,
-                                                      NULL);
-   gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  /* Second column.. title of the book. */
+  renderer = gtk_cell_renderer_text_new ();
+  column = gtk_tree_view_column_new_with_attributes ("Recipe",
+                                                     renderer,
+                                                     "text", TITLE_COLUMN,
+                                                     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
 
-   /* Last column.. whether a book is checked out. */
-   renderer = gtk_cell_renderer_text_new ();
-   column = gtk_tree_view_column_new_with_attributes ("Dietarry Requirement",
-                                                      renderer,
-                                                      "text", DIETARRY_COLUMN,
-                                                      NULL);
-   gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  /* Last column.. whether a book is checked out. */
+  renderer = gtk_cell_renderer_text_new ();
+  column = gtk_tree_view_column_new_with_attributes ("Dietarry Requirement",
+                                                     renderer,
+                                                     "text", DIETARRY_COLUMN,
+                                                     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
 
   gtk_grid_attach(GTK_GRID(grid), tree, 0, 0 , 5, 5);
 
@@ -216,6 +261,7 @@ static void activate (GtkApplication *app, gpointer user_data)
 
 int main (int argc, char **argv)
 {
+  printf("Helllo world");
   GtkApplication *app;
   int status;
 
